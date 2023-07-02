3af0b88978da6f5d6886d000d7e654de857704a6
naufalF21 <naufalfirman098@gmail.com>
2023-07-02 12:44:59 +0700

    add view table barang

--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -1,3 +1,58 @@
 @extends('layouts.main')
 @section('container')
+    <main>
+        <div class="container-fluid">
+            <h3 class="mt-4">Barang</h3>
+            <p>List master barang komputer</p>
+            <hr>
+            {{-- table --}}
+            <div class="card mb-4">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table" id="dataTable" width="100%" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th>Nama Barang</th>
+                                    <th>Kode Barang</th>
+                                    <th>Kategori</th>
+                                    <th>Stok</th>
+                                    <th>Harga Jual</th>
+                                    <th>Harga Beli</th>
+                                </tr>
+                            </thead>
+                            <tfoot>
+                                <tr>
+                                    <th>Nama Barang</th>
+                                    <th>Kode Barang</th>
+                                    <th>Kategori</th>
+                                    <th>Stok</th>
+                                    <th>Harga Jual</th>
+                                    <th>Harga Beli</th>
+                                </tr>
+                            </tfoot>
+                            <tbody>
+                                <tr>
+                                    <td>Tiger Nixon</td>
+                                    <td>System Architect</td>
+                                    <td>Edinburgh</td>
+                                    <td>61</td>
+                                    <td>2011/04/25</td>
+                                    <td>$320,800</td>
+                                </tr>
+                                <tr>
+                                    <td>Garrett Winters</td>
+                                    <td>Accountant</td>
+                                    <td>Tokyo</td>
+                                    <td>63</td>
+                                    <td>2011/07/25</td>
+                                    <td>$170,750</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            {{-- end table --}}
+        </div>
+    </main>
 @endsection
